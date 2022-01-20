9e098ded63ac3a0bd6ad2a501ecf4f789dede6b0
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-01-20 09:06:17 +0300

    IOS-6 Fixed bugs

--- ScrollViewProject/ScrollViewProject/ScrollViewController.swift
+++ ScrollViewProject/ScrollViewProject/ScrollViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ScrollViewController: UIViewController , UIScrollViewDelegate {
     @IBOutlet weak var pageControl: UIPageControl!
-    
     @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
@@ -28,26 +28,26 @@ class ScrollViewController: UIViewController , UIScrollViewDelegate {
  
     func createViews(){
         
-        let view1 = UIView()
-        view1.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 818)
-        view1.backgroundColor = .red
-        scrollView.addSubview(view1)
-        let view2 = UIView()
-        view2.frame = CGRect(x: view.frame.width, y: 0, width: view.frame.width*2, height: 818)
-        view2.backgroundColor = .systemPink
-        scrollView.addSubview(view2)
-        let view3 = UIView()
-        view3.frame = CGRect(x: view.frame.width*2, y: 0, width: view.frame.width*3, height: 818)
-        view3.backgroundColor = .green
-        scrollView.addSubview(view3)
-        let view4 = UIView()
-        view4.frame = CGRect(x: view.frame.width*3, y: 0, width: view.frame.width*4, height: 818)
-        view4.backgroundColor = .yellow
-        scrollView.addSubview(view4)
-        let view5 = UIView()
-        view5.frame = CGRect(x: view.frame.width*4, y: 0, width: view.frame.width*5, height: 818)
-        view5.backgroundColor = .systemBlue
-        scrollView.addSubview(view5)
+        let mintView = UIView()
+        mintView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 818)
+        mintView.backgroundColor = .systemMint
+        scrollView.addSubview(mintView)
+        let pinkView = UIView()
+        pinkView.frame = CGRect(x: view.frame.width, y: 0, width: view.frame.width*2, height: 818)
+        pinkView.backgroundColor = .systemPink
+        scrollView.addSubview(pinkView)
+        let purpleView = UIView()
+        purpleView.frame = CGRect(x: view.frame.width*2, y: 0, width: view.frame.width*3, height: 818)
+        purpleView.backgroundColor = .systemPurple
+        scrollView.addSubview(purpleView)
+        let orangeView = UIView()
+        orangeView .frame = CGRect(x: view.frame.width*3, y: 0, width: view.frame.width*4, height: 818)
+        orangeView .backgroundColor = .systemOrange
+        scrollView.addSubview(orangeView)
+        let indigoView = UIView()
+        indigoView.frame = CGRect(x: view.frame.width*4, y: 0, width: view.frame.width*5, height: 818)
+        indigoView.backgroundColor = .systemIndigo
+        scrollView.addSubview(indigoView)
         
     }
     func configurePageControl() {
@@ -67,7 +67,7 @@ class ScrollViewController: UIViewController , UIScrollViewDelegate {
     @objc func animateScrollView() {
         let scrollWidth = scrollView.bounds.width
         let currentXOffset = scrollView.contentOffset.x
-        
+        print(scrollWidth)
         let lastXPos = currentXOffset + scrollWidth
         if lastXPos != scrollView.contentSize.width {
             print("Scroll")
@@ -80,10 +80,8 @@ class ScrollViewController: UIViewController , UIScrollViewDelegate {
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print(scrollView.contentOffset)
-        pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(428))
+      //  print(scrollView.contentOffset)
+        let scrollWidth = scrollView.bounds.width
+        pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(scrollWidth))
     }
-
- 
-
 }
